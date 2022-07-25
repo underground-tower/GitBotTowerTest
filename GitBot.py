6e20845d0606a9f1906eeddaf871de0bb6b6f1b0
underground-tower <83032699+underground-tower@users.noreply.github.com>
2022-07-25 19:39:41 +0500

    Update GitBot.py

--- GitBot.py
+++ GitBot.py
@@ -2,10 +2,10 @@
 #pip3 install PyGithub requests
 #python -m pip install --upgrade pip
 from github import Github
-import time
+import time,github
 import bs4, requests
-g = Github('XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX')
-
+g = Github('XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX')
+#github.enable_console_debug_logging()
 def CreateFile(filename="file_name.txt",filedata="filedata",commit="",userName="underground-tower",repoName="GitBotTowerTest"):
 	global g
 	user = g.get_user(userName)
@@ -19,25 +19,26 @@ def DeleteFile(filename="file_name.txt",userName="underground-tower",repoName="G
 	contents = repo.get_contents(filename, ref="main")
 	repo.delete_file(contents.path, filename, contents.sha, branch="main")
 
-while 0:
-	try:
-		DeleteFile()
-	except Exception as e:
-		print("EROR Delite:    ",e)
+def UpdateFile(filedata="Filedata",commit="",filename="file_name.txt",userName="underground-tower",repoName="GitBotTowerTest"):
+	global g
+	user = g.get_user(userName)
 	
-	time.sleep(10)
-	try:
-		CreateFile()
-	except Exception as e:
-		print("EROR Create:    ",e)	
-	time.sleep(60)
+	repo = user.get_repo(repoName)
+	contents = repo.get_contents(filename, ref="main")
 
-	print("Update : ",time.time())
+	repo.update_file(contents.path, commit, filedata, contents.sha, branch="main")
 
+#UpdateFile()
+
+ip="0.0.0.0"
 while 1:
 
-	s = requests.get('https://2ip.ua/ru/')
+	s = requests.get('https://ifconfig.me/')
 	b = bs4.BeautifulSoup(s.text, "html.parser")
-	a = b.select(" .ipblockgradient .ip")[0].getText()
-	print(a)
-	time.sleep(1)
+
+	if(ip!=b):
+		print("New ip",b, end="r")
+		UpdateFile(filedata=str(b))
+	time.sleep(10)
+
+
